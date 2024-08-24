89ffb0142fbfdcd08acd11620eb1beef0d69e078
RizTrey <150064602+RizTrey@users.noreply.github.com>
2024-08-24 21:25:11 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div><img src="https://wallpapers.com/images/hd/clashof-clans-shield-logo-5806mjoghuxkzskf-2.png" className='w-12'/></div>
+  )
+}
+
+export default Logo
